c099412e3505667150526658d8a89a615cccf887
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2025-02-12 21:23:20 -0500

    index.js

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+// navigation bar, hamburger and X bar
+let navC = document.querySelector(".nav-container");
 let hBar = document.querySelector(".bar");
+let closeBar = document.querySelector(".close-bar");
+
+
 
 hBar.addEventListener("click", () => {
-    pass;
+    hBar.classList.add("hidden");
+    navC.classList.add("active");
+    closeBar.classList.remove("hidden");
 });
+
+closeBar.addEventListener("click", () => {
+    hBar.classList.remove("hidden");
+    navC.classList.remove("active");
+    closeBar.classList.add("hidden");
+})
