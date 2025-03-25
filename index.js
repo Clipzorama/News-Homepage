d31db190be80b6eb2369112fef22bb777c5f4ed3
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2025-03-24 22:32:03 -0400

    changing background overlay

--- index.js
+++ index.js
@@ -9,13 +9,11 @@ hBar.addEventListener("click", () => {
     hBar.classList.add("hidden");
     navC.classList.add("active");
     closeBar.classList.remove("hidden");
-    document.body.classList.add("dimmed");
 });
 
 closeBar.addEventListener("click", () => {
     hBar.classList.remove("hidden");
     navC.classList.remove("active");
     closeBar.classList.add("hidden");
-    document.body.classList.remove("dimmed");
 
 })
